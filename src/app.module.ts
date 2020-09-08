4e4b10e503ec29639c1525980c8653ffc748c442
dev3dmitriy.d <dev3dmitriy.d@alldigitalads.com>
2020-09-08 22:29:55 +0300

    install/connect postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskManagerModule } from './task-manager/task-manager.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskManagerModule],
+  imports: [
+      TaskManagerModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'root',
+        database: 'test',
+        entities: [],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
